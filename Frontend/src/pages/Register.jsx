10b4728fbf634eae87a6595a2a4f962014650be4
Adrian <adrian2000tg@gmail.com>
2026-04-16 12:05:48 +0200

    Stuffs

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./register.css";
+import "./Register.css";
 
 export default function Register({ onRegister, goLogin }) {
   const [username, setUsername] = useState("");
